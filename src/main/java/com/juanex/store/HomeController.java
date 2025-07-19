47c879df844d94c847b51283131095dd59de4c71
Juan Guevara M. <juanexgue@gmail.com>
2025-07-18 22:51:12 -0500

    Configuraciones iniciales, properties, maven

--- src/main/java/com/juanex/store/HomeController.java
+++ src/main/java/com/juanex/store/HomeController.java
@@ -1,4 +1,23 @@
 package com.juanex.store;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+
+    @RequestMapping("/")
+    public String index(){
+        String viewName = getViewName();
+        System.out.println("AppName:" + appName);
+        return viewName;
+    }
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    private String getViewName(){
+        return  "index.html";
+    }
 }
